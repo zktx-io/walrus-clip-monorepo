28156f1c34c0d7f752eb4b0a98fa07878e7097f4
Lim Jet <57783762+daoauth@users.noreply.github.com>
2025-01-27 23:41:54 +0900

    Update App.tsx

--- packages/example/src/App.tsx
+++ packages/example/src/App.tsx
@@ -12,7 +12,7 @@ import '@mysten/dapp-kit/dist/index.css';
 import { Home } from './pages/Home';
 import { Auth } from './pages/Auth';
 import { ICON, WALLET_NAME } from './utils/config';
-import { getProviderUrl } from './utils/getProviderUrl';
+// import { getProviderUrl } from './utils/getProviderUrl';
 import { NETWORK } from './utils/config';
 
 const router = createBrowserRouter([
@@ -28,18 +28,20 @@ const router = createBrowserRouter([
 
 const ENOKI_KEY = import.meta.env.VITE_APP_ENOKI_KEY;
 const SPONSORED_URL = import.meta.env.VITE_APP_SPONSORED_URL;
-const CLIENT_ID = import.meta.env.VITE_APP_CLIENT_ID;
+// const CLIENT_ID = import.meta.env.VITE_APP_CLIENT_ID;
 
 function App() {
   const [activeNetwork, setActiveNetwork] = useState<
     'testnet' | 'mainnet' | 'devnet'
   >(NETWORK);
 
+  /*
   const callbackNonce = (nonce: string) => {
     if (nonce && CLIENT_ID) {
       window.location.replace(getProviderUrl(nonce, CLIENT_ID));
     }
   };
+  */
 
   return (
     <KinokoWallet
