b65ad683c459e97292c5d259a58eb4f278137c42
Lim Jet <57783762+daoauth@users.noreply.github.com>
2025-02-21 00:38:41 +0900

    Update Kiosk.tsx

--- packages/demo/src/pages/Kiosk.tsx
+++ packages/demo/src/pages/Kiosk.tsx
@@ -46,11 +46,11 @@ export const Kiosk = () => {
           ),
         ],
       });
-      const { digest } = await pay('Pay', 'Please scan the QR code to pay.', {
-        transaction,
+      const txResult = await pay('Pay', 'Please scan the QR code to pay.', {
+        transactions: [transaction],
         isSponsored: true,
       });
-      enqueueSnackbar(digest, {
+      enqueueSnackbar(txResult[0].digest, {
         variant: 'success',
         style: {
           whiteSpace: 'nowrap',
